f49b3f784509fd4693bdec7cc1679b3be2ab6181
Denar Terrazas <denartp@gmail.com>
2019-04-14 14:41:25 -0400

    Android - Micruber
    Pantalla de Login de la aplicación

---
+++ dev/Android/Micruber/app/src/main/java/com/example/micruber/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.micruber;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+}
+

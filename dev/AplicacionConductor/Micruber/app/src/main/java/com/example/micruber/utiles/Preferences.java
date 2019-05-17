5d234f14d66812d0374be7aaf6b123433f45b3ba
Franz Bascope <franzbascope123@gmail.com>
2019-05-16 20:46:09 -0400

    aplicacion conductor para enviar coordenadas

---
+++ dev/AplicacionConductor/Micruber/app/src/main/java/com/example/micruber/utiles/Preferences.java
@@ -0,0 +1,73 @@
+package com.example.micruber.utiles;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.micruber.Objetos.Usuario;
+import com.example.micruber.Objetos.Vehiculo;
+import com.google.gson.Gson;
+
+
+public class Preferences {
+
+    public static void setUsuario(Context context, Usuario obj) {
+        SharedPreferences preferencias2 = context.getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor2 = preferencias2.edit();
+        Gson gson = new Gson();
+
+        editor2.putString("usuario_logeado", gson.toJson(obj));
+        editor2.commit();
+    }
+
+    public static Usuario getUsuario(Context context) {
+        SharedPreferences pref = context.getSharedPreferences("myPref", context.MODE_PRIVATE);
+        String objJson = pref.getString("usuario_logeado", "");
+        if (objJson.length() <= 0) {
+            return null;
+        } else {
+            Gson gson = new Gson();
+            Usuario obj = (Usuario) gson.fromJson(objJson, Usuario.class);
+            return obj;
+        }
+    }
+
+    public static void deleteUsuario(Context context) {
+        SharedPreferences preferencias2 = context.getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor2 = preferencias2.edit();
+        Gson gson = new Gson();
+
+        editor2.putString("usuario_logeado", "");
+        editor2.commit();
+    }
+
+    public static void setVehiculo(Context context, Vehiculo obj) {
+        SharedPreferences preferencias2 = context.getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor2 = preferencias2.edit();
+        Gson gson = new Gson();
+
+        editor2.putString("current_vehiculo", gson.toJson(obj));
+        editor2.commit();
+    }
+
+    public static Vehiculo getVehiculo(Context context) {
+        SharedPreferences pref = context.getSharedPreferences("myPref", context.MODE_PRIVATE);
+        String objJson = pref.getString("current_vehiculo", "");
+        if (objJson.length() <= 0) {
+            return null;
+        } else {
+            Gson gson = new Gson();
+            Vehiculo obj = (Vehiculo) gson.fromJson(objJson, Vehiculo.class);
+            return obj;
+        }
+    }
+
+    public static void deleteVehiculo(Context context) {
+        SharedPreferences preferencias2 = context.getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor2 = preferencias2.edit();
+        Gson gson = new Gson();
+
+        editor2.putString("current_vehiculo", "");
+        editor2.commit();
+    }
+
+}
---
+++ dev/AplicacionConductor/Micruber/app/src/main/java/com/example/micruber/utiles/Util.java
@@ -0,0 +1,48 @@
+package com.example.micruber.utiles;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+import com.example.micruber.R;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+public class Util {
+
+    public static String primeraMayuscula(String texto) {
+        if (texto.isEmpty()) {
+            return "";
+        }
+
+        return texto.substring(0,1).toUpperCase() + texto.substring(1).toLowerCase();
+    }
+
+    public static void mostrarDialogoSinInternet(Context context) {
+        final Dialog dialog = new Dialog(context);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_no_internet);
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        ((AppCompatButton) dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+    }
+
+}

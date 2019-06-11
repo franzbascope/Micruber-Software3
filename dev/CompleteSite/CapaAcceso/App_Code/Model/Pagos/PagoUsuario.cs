16710a63d880ae768b8ac8d9e1a47d8a53fa7094
erickCarp <43548116+erickCarp@users.noreply.github.com>
2019-06-11 18:57:33 -0400

    Model Usuario
    Objeto para pagos

---
+++ dev/CompleteSite/CapaAcceso/App_Code/Model/Pagos/PagoUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaAcceso.App_Code.Model.Pagos
+{
+    public class PagoUsuario
+    {
+        public String correo { get; set; }
+        public int monto { get; set; }
+        public int userRecargo { get; set; }
+
+    }
+}

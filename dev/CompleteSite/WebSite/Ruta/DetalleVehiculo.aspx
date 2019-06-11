49e2a5f20899830ab953e66d6432829fd1384dc7
Luis Humberto Navarro <ribeiro.beton@gmail.com>
2019-06-11 02:00:28 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ dev/CompleteSite/WebSite/Ruta/DetalleVehiculo.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DetalleVehiculo.aspx.cs" Inherits="Ruta_DetalleVehiculo" MasterPageFile="~/MasterPages/MasterPage.master"  %>
+
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" runat="Server">
+    
+    <asp:GridView runat="server" ID="AsignarLineaGridView" CssClass="table table-striped" GridLines="None" AutoGenerateColumns="false"
+                 >
+                <Columns>
+                    <asp:TemplateField HeaderText="Asignar">
+                        
+                        <ItemTemplate>
+                            <asp:CheckBox  runat="server" ID="chkRow"  Checked='<%# Eval("perteneceLinea") %>' />
+                       
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField HeaderText="LineaId"  DataField="lineaId" />
+                    <asp:BoundField HeaderText="NumeroLinea" DataField="numeroLinea" />
+                </Columns>
+            </asp:GridView>
+    
+        <div class="form-group">
+                        <asp:LinkButton ID="SaveButton" runat="server" OnClick="SaveButton_Click"
+                            CssClass="btn btn-primary"
+                            ValidationGroup="Usuario">
+                    Guardar
+                        </asp:LinkButton>
+                        <asp:HyperLink runat="server" NavigateUrl="~/Ruta/ListaVehiculos.aspx" CssClass="btn">
+                    Cancelar
+                        </asp:HyperLink>
+        </div>
+         <asp:HiddenField ID="RoleIdHiddenField" runat="server" Value="0" />
+
+    </asp:Content>
---
+++ dev/CompleteSite/WebSite/Ruta/DetalleVehiculo.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VentasNur.Model;
+
+public partial class Ruta_DetalleVehiculo : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!IsPostBack)
+            ProcesarParametros();
+        else
+        {
+            return;
+        }
+
+        try
+        {
+            // List<Permiso> list = PermisoBLL.getAllPermisos();
+            List<Linea> list = LineaBLL.getAllLineasByVehiculoId(VehiculoId);
+            AsignarLineaGridView.DataSource = list;
+            AsignarLineaGridView.DataBind();
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+    public int VehiculoId
+    {
+        set { RoleIdHiddenField.Value = value.ToString(); }
+        get
+        {
+            int VehiculoId = 0;
+            try
+            {
+                VehiculoId = Convert.ToInt32(RoleIdHiddenField.Value);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return VehiculoId;
+        }
+    }
+
+
+    private void ProcesarParametros()
+    {
+        if (Request.QueryString["VehiculoId"] != null && !string.IsNullOrEmpty(Request.QueryString["VehiculoId"]))
+        {
+            try
+            {
+                VehiculoId = Convert.ToInt32(Request.QueryString["VehiculoId"]);
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+    protected void SaveButton_Click(object sender, EventArgs e)
+    {
+        foreach (GridViewRow row in AsignarLineaGridView.Rows)
+        {
+            CheckBox chkRow = (row.Cells[0].FindControl("chkRow") as CheckBox);
+            if (chkRow.Checked)
+            {
+                try
+                {
+                    VehiculoBLL.insertLineaVehiculo(Convert.ToInt32(row.Cells[1].Text), VehiculoId);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("salio mal samierda");
+                    return;
+                }
+            }
+        }
+        Response.Redirect("ListaVehiculos.aspx");
+        return;
+    }
+
+   
+}

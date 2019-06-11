89fdf7860ffcfe76ec1b424bc6e31c250f168868
erickCarp <43548116+erickCarp@users.noreply.github.com>
2019-06-11 19:15:44 -0400

    Actualizando controlador pagos
    Actualizando controlador pagos

--- dev/CompleteSite/Servicios/Controllers/PagosController.cs
+++ dev/CompleteSite/Servicios/Controllers/PagosController.cs
@@ -22,34 +22,30 @@ namespace Servicios.Controllers
 
         [HttpPost()]
         [Route("obtenerPagos")]
-        public HttpResponseMessage PostPedido([FromBody]JToken body) 
+        public HttpResponseMessage PostPedido([FromBody]JToken body)
         {
             int usuarioId = body.Value<int>("usuarioId");
-            string fechaDesde = body.Value<string>("fechaDesde");
-            string fechaHasta = body.Value<string>("fechaHasta");
-            DateTime dateDesde = DateTime.ParseExact(fechaDesde, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-            DateTime dateHasta = DateTime.ParseExact(fechaHasta, "yyyy-MM-dd", CultureInfo.InvariantCulture);
             try
             {
-                List<Pagos> listaPagos = PagosBLL.getPagosByUsuarioId(usuarioId, dateDesde, dateHasta);
-                return Request.CreateResponse<List<Pagos>>(HttpStatusCode.OK, listaPagos);
+                List<PagoUsuario> listaPagos = PagosBLL.getPagosMontoByUsuarioId(usuarioId);
+                return Request.CreateResponse<List<PagoUsuario>>(HttpStatusCode.OK, listaPagos);
             }
             catch (Exception ex)
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
             }
         }
+
         [HttpPost()]
         [Route("pagos")]
-        public HttpResponseMessage PostPagos([FromBody]Pagos pago)
+        public HttpResponseMessage PostPagos([FromBody]PagoUsuario pago)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
-                    PagosBLL.insertPago(pago.usuarioId, pago.vehiculoId, pago.lineaId);
-                    Usuario objUsuario = UsuarioBLL.getUsuarioById(pago.usuarioId);
-                    return Request.CreateResponse<Usuario>(HttpStatusCode.OK, objUsuario);
+                    PagosBLL.insertPago(pago.correo, pago.monto, pago.userRecargo);
+                    return Request.CreateResponse<int>(HttpStatusCode.OK, 1);
                 }
             }
             catch (Exception ex)
